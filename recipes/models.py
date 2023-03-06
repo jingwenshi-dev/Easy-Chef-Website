27468134e37c64ff40f7723e860288c38a33be30
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-03-06 16:33:20 -0500

    Declared models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from accounts.models import User
+
+
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='user', default=None)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    # A list of ingredients and steps will be stored as JSON object
+    ingredients = models.JSONField()
+    steps = models.JSONField()
+    time = models.CharField()
+    cuisine = models.CharField()
+    diet = models.CharField()
