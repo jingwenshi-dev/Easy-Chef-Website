695a960d6ef4ab2ab6968600be98c6e6eca42000
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-03-09 15:07:21 -0500

    Added models in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from recipes.models import Recipe, Rating, Comment, LikedRecipe, BrowsedRecipe, ShoppingList
+from recipes.models import Recipe, Step, Ingredient,  Rating, Comment, LikedRecipe, BrowsedRecipe, ShoppingList
 
 # Register your models here.
 admin.site.register(Recipe)
+admin.site.register(Step)
+admin.site.register(Ingredient)
 admin.site.register(Rating)
 admin.site.register(Comment)
 admin.site.register(LikedRecipe)
