1492408b2c7b6881fba584ebfd6707b0c78c5510
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-03-11 23:25:47 -0500

    Declared all class based functionality needed

--- userfunction/views.py
+++ userfunction/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+class PopularRecipes():
+    """
+    Return a list of recipes based on its overall rating or the number of users marked them as favorite.
+    Note: You may want to use pagination.
+    """
+    # TODO
+    pass
+
+
+class MyRecipe():
+    """
+    Return three list of recipes: Created, Liked, Interacted (create, like, rate, or comment)
+    Note: You may want to use pagination.
+    """
+    # TODO
+    pass
+
+
+class Serach():
+    """
+    Search recipes by name, ingredients, or creator.
+    Then filter by cuisine, diet, or cooking time if these parameters are given.
+    Finally, rank the recipes by their ratings and likes.
+    Note: You may want to use pagination.
+    """
+    # TODO
+    pass
+
+
+class IngredientAutocomplete():
+    # TODO
+    pass
+
+
+class DisplayShoppingList():
+    """
+    Return combined ingredients and corresponding combined amount with unit.
+    """
+    # TODO
+    pass
+
